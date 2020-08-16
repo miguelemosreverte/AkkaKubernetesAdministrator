06eafbb0cc6ffe026ddce50d751a441ff65d2665
miguelemosreverte <miguelemosreverte@gmail.com>
2020-08-16 02:23:48 -0300

    Added apt-get install gettext-base

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-apt-get install -y kubectl kafkacat git curl unzip zip 
+apt-get install -y kubectl kafkacat git curl unzip zip gettext-base
 
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
@@ -16,7 +16,6 @@ sdk install java
 pip install awscli
 curl https://get.docker.com/ | sh
 service docker start
-
 
 
 
